231e17890de7e6d1dcd559f744c112081b1c1712
rh0 <125304115+rehite0@users.noreply.github.com>
2026-05-15 19:37:04 +0530

    fixed cursor

--- doth/tui.h
+++ doth/tui.h
@@ -1,5 +1,9 @@
 #pragma once
-struct rgb{char r; char g; char b;};
+struct rgb{short r; short g; short b;};
+struct C_PAIR{
+	struct rgb fg;
+	struct rgb bg;
+};
 struct win{
 	int x;
 	int y;
@@ -10,11 +14,11 @@ struct win{
 extern struct TYPE_PAGE{
 	struct win challenge_space;
 	struct win type_space;
-	struct rgb color_wrong;
-	struct rgb color_correct;
-	struct rgb color_untyped;
-	struct rgb color_cursor;
-	unsigned char* cursor_char;
+	struct C_PAIR color_wrong;
+	struct C_PAIR color_correct;
+	struct C_PAIR color_untyped;
+	// struct C_PAIR color_cursor;
+	// unsigned char* cursor_char;
 } type_page;
 struct result_page{
 	int a;
--- tui.c
+++ tui.c
@@ -11,6 +11,14 @@ void tui_main(){
 	initscr();
 	noecho();
 	cbreak();
+	start_color();
+	init_color(11,555,256,0);
+	init_color(12,555,555,20);
+	init_color(12,type_page.color_correct.bg.r
+			,type_page.color_correct.bg.g
+			,type_page.color_correct.bg.b);
+
+	init_pair(11,11,12);
 
 	WINDOW* challange_space_win= newwin(
 			type_page.challenge_space.h
@@ -28,7 +36,7 @@ void tui_main(){
 	box(challange_space_win,0,0);
 	box(type_space_win,0,0);
 
-	// mvwprintw(type_space_win,1,1,"qwre");
+	mvwprintw(challange_space_win,2,1,"qwre cs:%d c:%d",COLOR_PAIRS,COLORS);
 	char* test_str="asgvaerhaethbtbhtrhh";
 	gb_init(test_str, (int)strlen(test_str), 256);
 	mvwprintw(challange_space_win,1,1,"%s",test_str);
@@ -38,35 +46,45 @@ void tui_main(){
 	wrefresh(type_space_win);
 
 	int ch=0;
-	while( (ch=wgetch(type_space_win)) ){
-		switch(ch){
-			case KEY_BACKSPACE: case KEY_DC: case 127: case 8:
-				gb_pop();
-
-			break; case 27: //escape
-				goto exit;
-			break; default:
-				gb_push((char)ch);
-		}
+	while(1){
 		char* str;
 		char* status;
 		gb_get_status(&str, &status);
 
 		wclear(type_space_win);
 		box(type_space_win,0,0);
+	wattron(type_space_win,COLOR_PAIR(11));
 		wmove(type_space_win,1,1);
 
-		int i=0;
+		int i=0,x=-1,y=-1;
 		while(str[i]){
-			wprintw(type_space_win,"%c",str[i]);
+			if(status[i]=='u'&& x==-1&& y==-1){
+				x=getcurx(type_space_win);
+				y=getcury(type_space_win);
+			}
+
+			waddch(type_space_win,(chtype)str[i]);
 			if(getcurx(type_space_win)+1==getmaxx(type_space_win))
 				wmove(type_space_win,getcury(type_space_win)+1,1);
 			++i;
 		}
+		if(!(x==-1 || y==-1))
+			wmove(type_space_win,y,x);
 		free(str);free(status);
-		curs_set(0);
-		wrefresh(type_space_win);
-		curs_set(1);
+	wattroff(type_space_win,COLOR_PAIR(11));
+
+		curs_set(0); wrefresh(type_space_win); curs_set(1);
+
+		/* input processing */
+		ch=wgetch(type_space_win);
+		switch(ch){
+			case KEY_BACKSPACE: case KEY_DC: case 127: /*BS ascii*/ case 8:
+				gb_pop();
+			break; case 27: /*escape*/ case '\n':
+				goto exit;
+			break; default:
+				gb_push((char)ch);
+		}
 	}
 	exit:
 	delwin(challange_space_win);
